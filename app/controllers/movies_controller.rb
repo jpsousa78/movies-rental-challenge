dcca26cb5c6eedc5c9d6845a021b9b29e43be1ff
John Brito <jpsousabr@gmail.com>
2024-11-18 13:41:24 -0300

    Preliminary analysis

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,26 +1,39 @@
 class MoviesController < ApplicationController
+  
+  # ERROR HANDLING: The entire controller needs failsafes for when requests fails or data is incomplete.
+
+  # Could `Movie.all` be potentially dangerous due to the size of the catalog?
+  # If rendering in the frontend becomes an issue, one improvement would be to create a priority queue
+  # and index the most relevant movies and genres first with pagination, updating the list with the remaining movies upon request
   def index
     @movies = Movie.all
     render json: @movies
   end
 
+  # More comments at the RecommendationEngine
   def recommendations
     favorite_movies = User.find(params[:user_id]).favorites
+    # No error treatment. What if user is not logged in, request fails to fetch or the user has no registered favorite movies?
     @recommendations = RecommendationEngine.new(favorite_movies).recommendations
     render json: @recommendations
   end
 
+  # Feature suggestino: implement a token-based authentication method so only customers and admins can see this list (privacy)
   def user_rented_movies
     @rented = User.find(params[:user_id]).rented
     render json: @rented
   end
 
+  # Does it work asynchronously? If so, migh have trouble when two customers order the same movie
+  # If User.find or Movie.find fail, the following operations create data inconsistency
   def rent
     user = User.find(params[:user_id])
     movie = Movie.find(params[:id])
-    movie.available_copies -= 1
+    movie.available_copies -= 1 # Does not check if there are available copies
     movie.save
     user.rented << movie
     render json: movie
   end
+
+  # No return_movie method??
 end
--- app/services/recommendation_engine.rb
+++ app/services/recommendation_engine.rb
@@ -2,6 +2,19 @@ class RecommendationEngine
   def initialize(favorite_movies)
     @favorite_movies = favorite_movies
   end
+  # LOGIC: Recommendations only work based by genre. That's too limited. We can crossmatch other factors that
+  # potentially indicate what constitutes a movie a customer might like, such as duration, director, movie release-dates which could 
+  # indicate a preference for image quality, trends based by the period the movie was filmed in, etc.
+  # Another potiential improvement is cross-matching data from users who also had a certain movie as their favorite and suggest
+  # other favorites from the people who also had said movie as their favorite since people are often shown to have similar tastes
+  
+  # PERFORMANCE: Prediction algorithms are known to take a lot of processing time, changing the aforementioned logic could help in some ways
+  # If recommendations by other users' taste are implemented, a much shorter list will be a source of calculations. Limiting for a
+  # specific ammount of users to search from and possibly crossmatching with favorite genres, there could be a much quicker
+  # recommendation feature, allowing the customer to recalculate if they deem it necessary. Of course, ideally recommendations
+  # should work and then there would not be a need to recalculate, but it opens up features like these with time limitations.
+
+  # ERROR HANDLING: Needs failsafes for when requests fails or data is incomplete.
 
   def recommendations
     movie_titles = get_movie_names(@favorite_movies)
